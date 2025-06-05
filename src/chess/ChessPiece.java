fd2e3d885ab138472434bd8bc5d7e16b4b83ad26
Peeter Dal Vitt <peeter1301@gmail.com>
2025-06-05 10:09:49 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -4,7 +4,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 
-public abstract class ChessPiece extends Piece{
+public abstract class ChessPiece extends Piece {
 
 	private Color color;
 
@@ -12,12 +12,16 @@ public abstract class ChessPiece extends Piece{
 		super(board);
 		this.color = color;
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
 	
-	protected boolean isThereOpponentPiece(Position position){
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
 	}
